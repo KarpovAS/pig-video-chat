236057f56f73fedcd17ae0bf2540ac193f501827
KarpovAS <ffishywork@gmail.com>
2021-12-21 12:41:36 +0300

    Configuration changes

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const peerServer = ExpressPeerServer(server, {
 app.use("/peerjs", peerServer);
 app.use(express.static("public"));
 
-mongoose.connect('mongodb+srv://admin:1234@cluster0.1prho.mongodb.net/pig-video-chat?retryWrites=true&w=majority', (err) => {
+mongoose.connect('mongodb+srv://admin:1234@cluster0.1prho.mongodb.net/pig-chat?retryWrites=true&w=majority', (err) => {
   console.log("Database connection ", err)
 })
 
